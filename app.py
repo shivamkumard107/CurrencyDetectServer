4ee8c937a4fff79aa96cfd69322ee0262bda1830
shivamkumar107 <shivam.kumard107@gmail.com>
2020-04-09 11:25:31 +0530

    added more training data

--- app.py
+++ app.py
@@ -54,4 +54,4 @@ def handle_request():
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", port=4555, debug=True)
--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 #!usr/bin/env python
+# -*- coding: utf-8 -*-
 # @Date:	2017-03-22
 
 # UE14CS348 Digital Image Processing Mini Project
